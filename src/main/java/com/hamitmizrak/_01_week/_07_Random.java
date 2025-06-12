b2fbe784c2f42b34b36c84ac8f39c5b1e542f2b0
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-12 21:36:41 +0300

    Math

---
+++ src/main/java/com/hamitmizrak/_01_week/_07_Random.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak._01_week;
+
+public class _07_Random {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/hamitmizrak/_02_week/_02_Scanner.java
@@ -0,0 +1,4 @@
+package com.hamitmizrak._02_week;
+
+public class _02_Scanner {
+}
