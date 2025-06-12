44fc205abd1e1af1651734c4396e763f469a67a8
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-12 20:03:18 +0300

    psvm

--- src/main/java/com/hamitmizrak/_01_week/_02_Psvm.java
+++ src/main/java/com/hamitmizrak/_01_week/_02_Psvm.java
@@ -1,5 +1,11 @@
 package com.hamitmizrak._01_week;
 
 public class _02_Psvm {
-    // psvm
+
+    // psvm  => TAB
+    public static void main(String[] args) {
+
+        // sout => TAB
+        System.out.println("Merhabalar Güzel İnsanlar.");
+    }
 }
