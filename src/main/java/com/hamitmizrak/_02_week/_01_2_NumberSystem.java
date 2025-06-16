0694fbdeabee614f5225f212797e697134726451
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-16 20:18:34 +0300

    NumberSystem

--- src/main/java/com/hamitmizrak/_02_week/_01_2_NumberSystem.java
+++ src/main/java/com/hamitmizrak/_02_week/_01_2_NumberSystem.java
@@ -2,7 +2,24 @@ package com.hamitmizrak._02_week;
 
 public class _01_2_NumberSystem {
 
+    // Number System (  // 2-8-10-16)
     public static void main(String[] args) {
+
+        // Binary(2) => 0,1
+        int binary=0b011;
+        System.out.println(binary);
+
+        // Octal(8)  => 0,1,2,3,4,5,6,7
+        int octal=011;
+        System.out.println(octal);
+
+        // Decimal(10) => 0,1,2,3,4,5,6,7,8,9
+        int decimal=44;
+        System.out.println(decimal);
+
+        // Hexadecimal(16) => 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+        int hexadecimal=0xAA;
+        System.out.println(hexadecimal);
 
     }
 }
