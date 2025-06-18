066c12be8956d441691cb3f8a9da580d208a48d4
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-18 21:13:28 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/_02_week/_10_1_Exception.java
@@ -0,0 +1,62 @@
+package com.hamitmizrak._02_week;
+
+import java.io.IOException;
+
+/*
+✅ Checked Exception Nedir?
+Java tarafından zorunlu tutulur. Derleyici sizi uyarır.
+
+📌 Örnekler:
+IOException → dosya erişimi
+SQLException → veritabanı hataları
+ParseException → tarih dönüşüm hatası
+ */
+
+
+
+/*
+✅ Unchecked Exception Nedir?
+Derleyici sizi zorlamaz, ama çalışma zamanında program çökerse hatayı gösterir.
+
+📌 Örnekler:
+ArithmeticException → 10 / 0
+NullPointerException
+ArrayIndexOutOfBoundsException
+NumberFormatException
+ */
+
+public class _10_1_Exception {
+
+    // try - catch - finally -  throws - throw
+    // throws : Sayın yazılımcı bu alanda xyz istisnaları gelebilir - throw
+    public static void exceptionTutorials() throws NullPointerException, IOException {
+        try {
+            int number = 8 / 0;
+        } catch (ArithmeticException arithmeticException) {
+            arithmeticException.printStackTrace();
+            //String message = arithmeticException.getMessage();
+            //System.out.println(message);
+        } catch (NullPointerException nullPointerException) { // catch (ArithmeticException | NullPointerException e)
+            nullPointerException.printStackTrace();
+        } finally {
+            System.out.println("database.close");
+        }
+
+        System.out.println("Son satır");
+    }
+
+
+    public static void handleMethod() throws IOException {
+        exceptionTutorials();
+    }
+
+
+    // PSVM
+    public static void main(String[] args) {
+        try{
+            handleMethod();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hamitmizrak/_02_week/_10_2_CustomException.java
@@ -0,0 +1,45 @@
+package com.hamitmizrak._02_week;
+
+/*
+✅ Checked Exception Nedir?
+Java tarafından zorunlu tutulur. Derleyici sizi uyarır.
+
+📌 Örnekler:
+IOException → dosya erişimi
+SQLException → veritabanı hataları
+ParseException → tarih dönüşüm hatası
+ */
+
+
+/*
+✅ Unchecked Exception Nedir?
+Derleyici sizi zorlamaz, ama çalışma zamanında program çökerse hatayı gösterir.
+
+📌 Örnekler:
+ArithmeticException → 10 / 0
+NullPointerException
+ArrayIndexOutOfBoundsException
+NumberFormatException
+ */
+
+public class _10_2_CustomException extends Exception {
+
+    // Constructor
+    public _10_2_CustomException(String message) {
+        super(message);
+    }
+}
+
+
+class MainTestException {
+    public static void main(String[] args) throws _10_2_CustomException {
+
+        try {
+            int a = 5 / 0;
+        } catch (Exception e) {
+            throw new _10_2_CustomException("Burada istisna var");
+        }
+    }
+
+
+}
