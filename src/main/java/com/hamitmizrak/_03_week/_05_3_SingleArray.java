decc6ed56a686e6b4c69315370f7ee0c3d9fd2b7
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-23 21:22:57 +0300

    List, ArrayList, Arrays.asList()

--- src/main/java/com/hamitmizrak/_03_week/_05_3_SingleArray.java
+++ src/main/java/com/hamitmizrak/_03_week/_05_3_SingleArray.java
@@ -9,6 +9,7 @@ public class _05_3_SingleArray {
 
     public static void main(String[] args) {
 
+        // Array
         Integer[] data= new Integer[9];
         data[0]=2;
         data[1]=1;
@@ -20,30 +21,24 @@ public class _05_3_SingleArray {
         // data[7]=8; // eklenmeyen elemana = 0
         data[8]=9;
 
-        System.out.println("\n*** for Iterative ***");
-        // for döngüsü (Iterative)
-        for (int i = 0; i <data.length ; i++) {
-            System.out.print(data[i]+" ");
-        }
+        // Iterative
 
-        System.out.println("\n*** foreach isimsel ***");
-        // foreach isimsel
-        for( Integer temp : data ){
-            System.out.print(temp+" ");
-        }
+        // forach isimsel
 
-        System.out.println("\n*** java 8 forEach ***");
         // Java 8 Stream forEach
-        //List<int[]> dataList= Arrays.asList(data);
-        // List<Integer> dataList= Arrays.asList(data).stream().forEach(System.out::print);
-        List<Integer> dataList=   Arrays.asList(data);
-        System.out.println(dataList);
+        // Array'i Collections çevir
+        System.out.println("\n*** java 8 forEach (stream.forEach) ***");
+        Arrays.asList(data).stream().forEach(System.out::println);
 
-        System.out.println("\n*** foreach sorting ***");
-        // Sıralama
-        Arrays.sort(data);
-        for( Integer temp : data ){
-            System.out.print(temp+" ");
-        }
+        System.out.println("\n*** java 8 forEach (stream.forEach.filter) ***");
+        Arrays.asList(data).stream().filter(e->e!=null).forEach(System.out::println);
+
+        System.out.println("\n*** java 8 forEach (forEach) ***");
+        Arrays.asList(data).forEach(System.out::println);
+
+        System.out.println("\n*** java 8 forEach (stream.forEach.Lambda) ***");
+        List<Integer> dataList= Arrays.asList(data);
+        dataList.stream().forEach(temp-> System.out.println("Elemanlar: "+temp));
+
     }
 }
