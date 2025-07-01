2364be76d66d6d5f21eb09deacfa2bfa3b2a72f2
Hamit Mizrak <hamitmizrak@gmail.com>
2025-07-01 19:16:55 +0300

    Lombok

--- src/main/java/com/hamitmizrak/_04_week/_01_2_Lombok_tutorials.java
+++ src/main/java/com/hamitmizrak/_04_week/_01_2_Lombok_tutorials.java
@@ -1,6 +1,7 @@
 package com.hamitmizrak._04_week;
 
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 
 // LOMBOK
@@ -9,6 +10,7 @@ import lombok.*;
 @RequiredArgsConstructor // Injection : Constructor
 @ToString
 @EqualsAndHashCode
+
 //@Data = Aşağıdakileri yerine geçiyor.
 //@Getter
 //@Setter
@@ -21,26 +23,28 @@ import lombok.*;
 @Builder  // Builder parametreli constructor eklemek zorundayız.
 //@Log4j
 //@Log4j2
+
+@FieldDefaults(level = AccessLevel.PRIVATE) // Erişim belirleyeiclerden hepsini private yap
+// @SneakyThrows //
 public class _01_2_Lombok_tutorials {
 
     // Field
-    @NonNull
-    private String name;
-    @NonNull
-    private String surname;
-    @NonNull
-    private Long number;
+    @NonNull String name;
+    @NonNull String surname;
+    @NonNull Long number;
 
     // Default için: 1.şart Builder 2.Builder'de parametreli constructor
     @Builder.Default
     private boolean isLogin=true;
 
+    // PSVM
     public static void main(String[] args) {
         _01_2_Lombok_tutorials tutorials=  _01_2_Lombok_tutorials
                 .builder()
                     .name("adı").surname("soyadı").number(44L)
                 .build();
 
+        //
         System.out.println(tutorials);
     }
 }
